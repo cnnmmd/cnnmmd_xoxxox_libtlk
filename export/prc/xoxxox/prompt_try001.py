50f21ab4e202536f75e3eba22f0638ff3b61d07c
azsgit <10238943+azsgit@users.noreply.github.com>
2025-09-06 18:50:01 +0900

    wip

---
+++ export/prc/xoxxox/prompt_try001.py
@@ -0,0 +1,40 @@
+#---------------------------------------------------------------------------
+# 参照
+
+import re
+
+#---------------------------------------------------------------------------
+# 構築：プロンプト：sys / usr / agt
+
+class ConLog:
+  strcon = ""
+  strlog = "\n"
+
+  @classmethod
+  def catsys(self, diccnf):
+    txtsys = diccnf["status"]
+    self.maxlog = diccnf["maxlog"] + 2
+    self.lstsys = []
+    self.lstlog = []
+    frmsys = self.strcon.join(["<|sys|>", txtsys, "<|end|>"])
+    self.lstsys.append(frmsys)
+
+  @classmethod
+  def catreq(self, txtreq):
+    frmusr = self.strcon.join(["<|usr|>", txtreq, "<|end|>"])
+    self.lstlog.append(frmusr)
+    txtlog = self.strlog.join(["".join(self.lstsys), self.strlog.join(self.lstlog), "<|agt|>"]) + self.strcon
+    return txtlog
+
+  @classmethod
+  def catres(self, txtres):
+    frmagt = self.strcon.join(["<|agt|>", txtres, "<|end|>"])
+    self.lstlog.append(frmagt)
+    if len(self.lstlog) > self.maxlog:
+      self.lstlog.pop(0)
+      self.lstlog.pop(0)
+
+  @classmethod
+  def arrres(self, txtifr):
+    txtres = re.sub(r"\s*<\|end\|>.*", "", txtifr, flags=re.DOTALL).strip()
+    return (txtres, "")
